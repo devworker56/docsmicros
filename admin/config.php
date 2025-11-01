861f51d28e36546ea96451466d73122e3215afac
devworker56 <alainkavena@gmail.com>
2025-11-01 14:04:21 -0400

    add some Debug the redirect in config.php

--- admin/config.php
+++ admin/config.php
@@ -1,6 +1,8 @@
 <?php
 // admin/config.php - Admin configuration
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once '../includes/config.php';
 require_once '../includes/database.php';
 
